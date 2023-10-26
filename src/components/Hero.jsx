42dbba4bb9d697003350e55278cc4dc5cf3b9f0b
Antonio <antonio.rodriguez024@gmail.com>
2023-10-25 21:29:21 -0500

    navbar added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import { discount, robot} from '../assets'
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
+  <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+
+  </section>
 }
 
 export default Hero
